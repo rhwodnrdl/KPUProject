45a01a020c5975bd6f8be49d1dc080ca5738bdd3
kojw <rhwodnrdl@naver.com>
2018-05-16 18:35:48 +0900

    3차 프로토타입 소스

---
+++ Prototype/Raspberry-Web,MQTT/MQTT/linux/Publish/publish.cpp
@@ -0,0 +1,267 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+
+
+char certDirectory[PATH_MAX + 1] = "../../../certs";
+char HostAddress[255] = AWS_IOT_MQTT_HOST;
+uint32_t port = AWS_IOT_MQTT_PORT;
+uint32_t publishCount = 0;
+
+void iot_subscribe_callback_handler(AWS_IoT_Client *pClient, char *topicName, uint16_t topicNameLen,
+									IoT_Publish_Message_Params *params, void *pData) {
+	IOT_UNUSED(pData);
+	IOT_UNUSED(pClient);
+	IOT_INFO("Subscribe callback");
+	IOT_INFO("%.*s\t%.*s", topicNameLen, topicName, (int) params->payloadLen, (char *) params->payload);
+}
+
+void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data) {
+	IOT_WARN("MQTT Disconnect");
+	IoT_Error_t rc = FAILURE;
+
+	if(NULL == pClient) {
+		return;
+	}
+
+	IOT_UNUSED(data);
+
+	if(aws_iot_is_autoreconnect_enabled(pClient)) {
+		IOT_INFO("Auto Reconnect is enabled, Reconnecting attempt will start now");
+	} else {
+		IOT_WARN("Auto Reconnect not enabled. Starting manual reconnect...");
+		rc = aws_iot_mqtt_attempt_reconnect(pClient);
+		if(NETWORK_RECONNECTED == rc) {
+			IOT_WARN("Manual Reconnect Successful");
+		} else {
+			IOT_WARN("Manual Reconnect Failed - %d", rc);
+		}
+	}
+}
+
+void parseInputArgsForConnectParams(int argc, char **argv) {
+	int opt;
+
+	while(-1 != (opt = getopt(argc, argv, "h:p:c:x:"))) {
+		switch(opt) {
+			case 'h':
+				strcpy(HostAddress, optarg);
+				IOT_DEBUG("Host %s", optarg);
+				break;
+			case 'p':
+				port = atoi(optarg);
+				IOT_DEBUG("arg %s", optarg);
+				break;
+			case 'c':
+				strcpy(certDirectory, optarg);
+				IOT_DEBUG("cert root directory %s", optarg);
+				break;
+			case 'x':
+				publishCount = atoi(optarg);
+				IOT_DEBUG("publish %s times\n", optarg);
+				break;
+			case '?':
+				if(optopt == 'c') {
+					IOT_ERROR("Option -%c requires an argument.", optopt);
+				} else if(isprint(optopt)) {
+					IOT_WARN("Unknown option `-%c'.", optopt);
+				} else {
+					IOT_WARN("Unknown option character `\\x%x'.", optopt);
+				}
+				break;
+			default:
+				IOT_ERROR("Error in command line argument parsing");
+				break;
+		}
+	}
+
+}
+
+void sigint(int sig){
+
+	unlink("./temp");
+	unlink("./humid");
+	exit(1);
+}
+
+int main(int argc, char **argv) {
+
+	char msgTemp[50];
+	char msgHumid[50];
+	int desTemp;
+	int desHumid;
+	int readTemp;
+	int readHumid;
+	
+	bool infinitePublishFlag = true;
+
+	char rootCA[PATH_MAX + 1];
+	char clientCRT[PATH_MAX + 1];
+	char clientKey[PATH_MAX + 1];
+	char CurrentWD[PATH_MAX + 1];
+
+	IoT_Error_t rc = FAILURE;
+
+	AWS_IoT_Client client;
+	IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
+	IoT_Client_Connect_Params connectParams = iotClientConnectParamsDefault;
+
+	IoT_Publish_Message_Params paramsTemp;
+	IoT_Publish_Message_Params paramsHumid;
+
+	parseInputArgsForConnectParams(argc, argv);
+
+	if(mkfifo("./temp",0666) == -1){
+
+        	printf("fail to call temp()\n");
+        	exit(1);
+	}
+    
+	if((desTemp = open("./temp", O_RDWR)) < 0){
+        	printf("fail to call temp()\n");
+        	exit(1);
+	}
+
+	if(mkfifo("./humid",0666) == -1){
+
+        	printf("fail to call temp()\n");
+        	exit(1);
+	}
+    
+	if((desHumid = open("./humid", O_RDWR)) < 0){
+        	printf("fail to call temp()\n");
+        	exit(1);
+	}
+
+	IOT_INFO("\nAWS IoT SDK Version %d.%d.%d-%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
+
+	getcwd(CurrentWD, sizeof(CurrentWD));
+	snprintf(rootCA, PATH_MAX + 1, "%s/%s/%s", CurrentWD, certDirectory, AWS_IOT_ROOT_CA_FILENAME);
+	snprintf(clientCRT, PATH_MAX + 1, "%s/%s/%s", CurrentWD, certDirectory, AWS_IOT_CERTIFICATE_FILENAME);
+	snprintf(clientKey, PATH_MAX + 1, "%s/%s/%s", CurrentWD, certDirectory, AWS_IOT_PRIVATE_KEY_FILENAME);
+
+	IOT_DEBUG("rootCA %s", rootCA);
+	IOT_DEBUG("clientCRT %s", clientCRT);
+	IOT_DEBUG("clientKey %s", clientKey);
+	mqttInitParams.enableAutoReconnect = false; // We enable this later below
+	mqttInitParams.pHostURL = HostAddress;
+	mqttInitParams.port = port;
+	mqttInitParams.pRootCALocation = rootCA;
+	mqttInitParams.pDeviceCertLocation = clientCRT;
+	mqttInitParams.pDevicePrivateKeyLocation = clientKey;
+	mqttInitParams.mqttCommandTimeout_ms = 20000;
+	mqttInitParams.tlsHandshakeTimeout_ms = 5000;
+	mqttInitParams.isSSLHostnameVerify = true;
+	mqttInitParams.disconnectHandler = disconnectCallbackHandler;
+	mqttInitParams.disconnectHandlerData = NULL;
+
+	rc = aws_iot_mqtt_init(&client, &mqttInitParams);
+	if(SUCCESS != rc) {
+		IOT_ERROR("aws_iot_mqtt_init returned error : %d ", rc);
+		return rc;
+	}
+
+	connectParams.keepAliveIntervalInSec = 600;
+	connectParams.isCleanSession = true;
+	connectParams.MQTTVersion = MQTT_3_1_1;
+	connectParams.pClientID = (char *)AWS_IOT_MQTT_CLIENT_ID;
+	connectParams.clientIDLen = (uint16_t) strlen(AWS_IOT_MQTT_CLIENT_ID);
+	connectParams.isWillMsgPresent = false;
+
+	IOT_INFO("Connecting...");
+	rc = aws_iot_mqtt_connect(&client, &connectParams);
+	if(SUCCESS != rc) {
+		IOT_ERROR("Error(%d) connecting to %s:%d", rc, mqttInitParams.pHostURL, mqttInitParams.port);
+		return rc;
+	}
+	/*
+	 * Enable Auto Reconnect functionality. Minimum and Maximum time of Exponential backoff are set in aws_iot_config.h
+	 *  #AWS_IOT_MQTT_MIN_RECONNECT_WAIT_INTERVAL
+	 *  #AWS_IOT_MQTT_MAX_RECONNECT_WAIT_INTERVAL
+	 */
+	rc = aws_iot_mqtt_autoreconnect_set_status(&client, true);
+	if(SUCCESS != rc) {
+		IOT_ERROR("Unable to set Auto Reconnect to true - %d", rc);
+		return rc;
+	}
+
+	IOT_INFO("Subscribing...");
+	rc = aws_iot_mqtt_subscribe(&client, "ws1/temp", 8, QOS0, iot_subscribe_callback_handler, NULL);
+	if(SUCCESS != rc) {
+		IOT_ERROR("Error subscribing : %d ", rc);
+		return rc;
+	}
+
+	rc = aws_iot_mqtt_subscribe(&client, "ws1/humid", 9, QOS0, iot_subscribe_callback_handler, NULL);
+	if(SUCCESS != rc) {
+		IOT_ERROR("Error subscribing : %d ", rc);
+		return rc;
+	}
+
+	paramsTemp.qos = QOS0;
+	paramsTemp.payload = (void *) msgTemp;
+	paramsTemp.isRetained = 0;
+
+	paramsHumid.qos = QOS0;
+	paramsHumid.payload = (void *) msgHumid;
+	paramsHumid.isRetained = 0;
+
+	if(publishCount != 0) {
+		infinitePublishFlag = false;
+	}
+
+	signal(SIGINT,sigint);
+
+	while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc)
+		  && (publishCount > 0 || infinitePublishFlag)) {
+
+		//Max time the yield function will wait for read messages
+		rc = aws_iot_mqtt_yield(&client, 100);
+		if(NETWORK_ATTEMPTING_RECONNECT == rc) {
+			// If the client is attempting to reconnect we will skip the rest of the loop.
+			continue;
+		}
+
+		if((readTemp = read(desTemp, msgTemp, 50)) < 0 ){
+			printf("fail to call read()\n");
+			exit(1);
+		}
+
+		if((readHumid = read(desHumid, msgHumid, 50)) < 0 ){
+			printf("fail to call read()\n");
+			exit(1);
+		}
+
+		sleep(1);
+		paramsTemp.payloadLen = strlen(msgTemp);
+		rc = aws_iot_mqtt_publish(&client, "ws1/temp", 8, &paramsTemp);
+		if(publishCount > 0) {
+			publishCount--;
+		}
+
+		paramsHumid.payloadLen = strlen(msgHumid);
+		rc = aws_iot_mqtt_publish(&client, "ws1/humid", 9, &paramsHumid);
+		if(publishCount > 0) {
+			publishCount--;
+		}
+	}
+
+	if(SUCCESS != rc) {
+		IOT_ERROR("An error occurred in the loop.\n");
+	} else {
+		IOT_INFO("Publish done\n"); 
+	}
+
+	return rc;
+}
